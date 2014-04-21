ac86e23e2a08588af0848cb2a138c0bc5daa905a
Jordan Nolan <jordanwn@gmail.com>
2014-04-21 14:00:33 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PepperMint::Application.config.secret_key_base = '1375ab785ebf84b2542955f10b58ac23ef24cfecc4f8ccc801370124a8c1bd4ec96eb9aa4b0b8457f39ace96e97007fbca8798187833ca6e42619bbd1537629d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PepperMint::Application.config.session_store :cookie_store, key: '_PepperMint_session'
