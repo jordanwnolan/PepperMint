ed6a38cee9e3f07448bc2a55fbdc9811d4e8a8ce
Jordan Nolan <jordanwn@gmail.com>
2014-04-26 11:09:24 -0400

    fixes some issues with budgets, budgets now in card

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -2,10 +2,9 @@ FREQUENCIES = {0 => 'Daily',
      1 => 'Weekly',
      2 => 'Monthly'}
 
-FREQUENCY_RESETS = {0 => [],
-     1 => ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-     2 => ['First of Month', '15th of Month', 'End of Month']
-   }
+WEEKLY_FREQUENCY_RESETS =  ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+MONTHLY_FREQUENCY_RESETS = ['First of Month', '15th of Month', 'End of Month']
 
    GENDER = {
     0 => 'M',
