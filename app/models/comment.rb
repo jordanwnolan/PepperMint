b25dc648cf32f01dc9067d56af67986d168abfe6
Jordan Nolan <jordanwn@gmail.com>
2014-04-28 21:43:32 -0400

    cleans up more views

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,7 @@
 class Comment < ActiveRecord::Base
   include Commentable
+  validates :body, presence: true
+  validate :author
   belongs_to :commentable, polymorphic: true
   belongs_to :author, class_name: "User"
 end
