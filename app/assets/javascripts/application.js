e80cf673c65574599c26e1875d85c0dd6ec811ca
Jordan Nolan <jordanwn@gmail.com>
2014-04-29 20:05:28 -0400

    undo backbone on master

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,11 +14,4 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require underscore
-//= require backbone
-//= require pepper_mint
-//= require_tree ../templates
-//= require_tree ./models
-//= require_tree ./collections
-//= require_tree ./views
-//= require_tree ./routers
 //= require_tree .
